d1a7175b8312e83c52ed51b4ecbb27a82ec55789
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-04-22 21:09:41 -0300

    Upgrade jquery sources

--- lib/yano/jquery/rails/version.rb
+++ lib/yano/jquery/rails/version.rb
@@ -2,10 +2,10 @@ module Yano
   module Jquery
     module Rails
       VERSION = "0.0.4"
-      JQUERY_VERSION = "1.12.1"
-      JQUERY_2_VERSION = "2.2.1"
+      JQUERY_VERSION = "1.12.3"
+      JQUERY_2_VERSION = "2.2.3"
       JQUERY_UI_VERSION = "1.11.4"
-      JQUERY_UJS_VERSION = "1.2.0"
+      JQUERY_UJS_VERSION = "1.2.1"
     end
   end
 end
