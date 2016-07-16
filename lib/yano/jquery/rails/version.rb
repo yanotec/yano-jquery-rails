43cee275a25314be09cd08e099ce15dd8279beab
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-07-16 07:51:54 -0300

    Bumping version 2.1.0

--- lib/yano/jquery/rails/version.rb
+++ lib/yano/jquery/rails/version.rb
@@ -1,7 +1,7 @@
 module Yano
   module Jquery
     module Rails
-      VERSION = "2.0.0"
+      VERSION = "2.1.0"
       JQUERY_VERSION = "3.1.0"
       JQUERY_UI_VERSION = "1.11.4"
       JQUERY_UJS_VERSION = "1.2.1"
