229dbb559254edb95aa4947b98c2fc18cdde3299
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-11-07 14:41:16 -0200

    Update jquery 3 version and jquery ui version

--- lib/yano/jquery/rails/version.rb
+++ lib/yano/jquery/rails/version.rb
@@ -4,8 +4,8 @@ module Yano
       VERSION = "2.1.3"
       JQUERY_VERSION = "1.12.4"
       JQUERY_2_VERSION = "2.2.4"
-      JQUERY_3_VERSION = "3.1.0"
-      JQUERY_UI_VERSION = "1.12.0"
+      JQUERY_3_VERSION = "3.1.1"
+      JQUERY_UI_VERSION = "1.12.1"
       JQUERY_UJS_VERSION = "1.2.2"
     end
   end
